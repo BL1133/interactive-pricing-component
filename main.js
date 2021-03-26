80993e734703954dbc9a934925d59137a7ba4f7d
Bryan Lee <bryan.lee@lynxcreations.com>
2021-03-25 22:42:17 -0400

    billing discount added to slider price

--- main.js
+++ main.js
@@ -20,11 +20,23 @@ pricingOutput.amount = pricingOutputEl.querySelector('.pricing__price-amount');
 pricingOutput.month = pricingOutputEl.querySelector('.pricing__price-month');
 pricingOutput.data = JSON.parse(pricingOutputEl.dataset.priceInput);
 
-handlePricingSlider(pricingInput, pricingOutput);
-window.addEventListener('input', () => {
+// Discount
+const toggleBilling = document.querySelector('.toggle-btn input');
+// Check billing input
+let stateBilling = 0;
+function checkBilling() {
+  if (toggleBilling.checked) {
+    stateBilling = 1;
+  } else {
+    stateBilling = 0;
+  }
+}
+toggleBilling.addEventListener('input', () => {
+  checkBilling();
   handlePricingSlider(pricingInput, pricingOutput);
 });
 
+// Bind input to Output
 function handlePricingSlider(input, output) {
   // Output the current slider value
   if (input.currentValEl) {
@@ -34,10 +46,17 @@ function handlePricingSlider(input, output) {
   if (output.currency) {
     output.currency.innerHTML = output.data[input.el.value][0];
   }
-  if (output.amount) {
+  if (output.amount && stateBilling == 0) {
     output.amount.innerHTML = output.data[input.el.value][1];
+  } else if (output.amount && stateBilling == 1) {
+    output.amount.innerHTML =
+      output.data[input.el.value][1] - output.data[input.el.value][1] * 0.25;
   }
   if (output.month) {
     output.month.innerHTML = output.data[input.el.value][2];
   }
 }
+handlePricingSlider(pricingInput, pricingOutput);
+pricingSlider.addEventListener('input', () => {
+  handlePricingSlider(pricingInput, pricingOutput);
+});
